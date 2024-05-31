bee3e28f849d9b2979fe3313577f1e33be4e8166
Samuel Sml <veramirandasamuel6@gmail.com>
2024-05-31 13:41:34 -0500

    add: create category.entity file

---
+++ src/modules/categories/entities/category.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 150 })
+  description: string;
+}
