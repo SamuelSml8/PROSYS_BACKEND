1c244b552e2bc9e73134e6ee05bedf9779a8ed9c
Samuel Sml <veramirandasamuel6@gmail.com>
2024-06-05 11:32:10 -0500

    refactor: update DTOs for consistency and validation enhancement
    This commit standardizes DTO property decorators for consistency, adds validation decorators like @IsNotEmpty() to ensure required fields are not empty, utilizes class-transformer for value transformation to trim whitespace, improves error messages for validation constraints, and adds Swagger API property decorators for better documentation.

--- src/modules/users/dtos/create-user.dto.ts
+++ src/modules/users/dtos/create-user.dto.ts
@@ -1,23 +1,47 @@
-import { IsString, IsEmail, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import {
+  IsString,
+  IsEmail,
+  Length,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @Length(1, 50)
+  @IsNotEmpty()
+  @Transform(({ value }) => value.trim())
+  @ApiProperty()
   name: string;
 
-  @IsEmail()
   @Length(1, 50)
+  @IsEmail()
+  @IsNotEmpty()
+  @Transform(({ value }) => value.toLowerCase())
+  @ApiProperty()
   email: string;
 
+  @IsNotEmpty()
   @IsString()
-  @Length(1, 100)
+  @MinLength(8, { message: 'The password must be at least 8 characters long' })
+  @MaxLength(50, {
+    message: 'The password must have maximum of 50 characters ',
+  })
+  @ApiProperty()
   password: string;
 
   @IsString()
   @Length(1, 15)
+  @Transform(({ value }) => value.trim())
+  @ApiProperty()
   telephone: string;
 
   @IsString()
   @Length(1, 10)
+  @Transform(({ value }) => value.trim())
+  @ApiProperty()
   role: string;
 }
