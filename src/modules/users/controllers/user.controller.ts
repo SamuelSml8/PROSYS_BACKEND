26b083142fd767748922569a01e2077af7d8b874
Samuel Sml <veramirandasamuel6@gmail.com>
2024-06-05 10:23:46 -0500

    chore(user-controller): Add Swagger documentation for User Controller
    Description:
    Updated the UserController to include Swagger annotations for API documentation. This includes detailed responses for each endpoint, specifying possible HTTP statuses and their descriptions. By integrating Swagger documentation, we enhance the API's usability and clarity for developers and clients interacting with the User module. This aligns with the goal of improving API documentation and making endpoints self-descriptive for seamless integration and testing.

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -10,36 +10,80 @@ import {
 import { UserService } from '../services/user.service';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UpdateUserDto } from '../dtos/update-user.dto';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiCreatedResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Create a user' })
   @Post('create')
   async createUser(@Body() user: CreateUserDto) {
     return this.userService.createUser(user);
   }
 
+  @ApiOkResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Find user by email' })
   @Get('find/email/:email')
   async findUserByEmail(@Param('email') email: string) {
     return this.userService.findUserByEmail(email);
   }
 
+  @ApiOkResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Find users by name' })
   @Get('find/name/:name')
   async findUsersByName(@Param('name') name: string) {
     return this.userService.findUsersByName(name);
   }
 
+  @ApiOkResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Find all users' })
   @Get('all')
   async findAllUsers() {
     return this.userService.findAllUsers();
   }
 
+  @ApiOkResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Update user' })
   @Put('update/:id')
   async updatedUser(@Param('id') id: number, @Body() user: UpdateUserDto) {
     return this.userService.updatedUser(id, user);
   }
 
+  @ApiOkResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Delete user' })
   @Delete('delete/:id')
   async deleteUser(@Param('id') id: number) {
     return this.userService.deleteUser(id);
