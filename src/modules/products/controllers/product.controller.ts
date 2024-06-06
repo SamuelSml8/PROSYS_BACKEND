8a7252a6e6b0a91a41968787feb9b7c91a7e2bac
Samuel Sml <veramirandasamuel6@gmail.com>
2024-06-06 13:55:39 -0500

    docs(product-module): add Swagger decorators for API documentation

--- src/modules/products/controllers/product.controller.ts
+++ src/modules/products/controllers/product.controller.ts
@@ -10,21 +10,49 @@ import {
 } from '@nestjs/common';
 import { ProductService } from '../services/product.service';
 import { CreateProductDto, UpdateProductDto } from '../dtos';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('Products')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ApiCreatedResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Create a product' })
   @Post('create')
   async create(@Body() product: CreateProductDto) {
     return this.productService.create(product);
   }
 
+  @ApiOkResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Find all products' })
   @Get('all')
   async findAll(@Query('page') page: number, @Query('limit') limit: number) {
     return this.productService.findAll(page, limit);
   }
 
+  @ApiOkResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Find products by name' })
   @Get('find/:name')
   async findProductsByName(
     @Param('name') name: string,
@@ -34,11 +62,22 @@ export class ProductController {
     return this.productService.findProductsByName(name, page, limit);
   }
 
+  @ApiOkResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Update category' })
   @Put('update/:id')
   async update(@Param('id') id: number, @Body() product: UpdateProductDto) {
     return this.productService.updateProduct(id, product);
   }
 
+  @ApiOkResponse({ description: 'Success' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({ description: 'Server Error' })
+  @ApiOperation({ summary: 'Delete category' })
   @Delete('delete/:id')
   async delete(@Param('id') id: number) {
     return this.productService.deleteProduct(id);
