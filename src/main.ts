647f77d99970fd2061a3a9edc8b6a55c7d5f4d2f
Samuel Sml <veramirandasamuel6@gmail.com>
2024-06-07 13:50:11 -0500

    docs(swagger): update Swagger documentation description with project details and team information
    - Added a comprehensive description of Prosys to the Swagger documentation.
    - Included project purpose, functionality, and target audience.
    - Listed the names of the students presenting the project.

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Prosys')
-    .setDescription('Prosys is a comprehensive system developed by SOMOS RETENES, designed to efficiently manage products, orders and customers. With an intuitive interface and powerful functionalities, Prosys simplifies inventory management, facilitates order tracking and optimizes customer interaction. From product administration to order management and customer service, Prosys offers a complete solution for SOMOS RETENES business needs. This Swagger documentation provides a detailed description of available endpoints, accepted parameters and expected responses, allowing easy integration and development of external applications with Prosys.')
+    .setDescription(
+      'Prosys is a comprehensive system developed for SOMOS RETENES, designed to efficiently manage products, orders and customers. Intuitive to use and with powerful functionality, Prosys simplifies inventory management, facilitates order tracking and optimizes customer interaction. From product management to order management and customer service, Prosys offers a complete solution for SOMOS RETENES business needs. \n\nThis Swagger documentation provides a detailed description of available endpoints, accepted parameters and expected responses, allowing easy integration and development of external applications with Prosys. This system will be presented as a final project of the work technique in Software Programming of the Universidad Pontificia Bolivariana by the students: \n\n- Samuel Vera Miranda \n\n- Juan Esteban Alcaraz',
+    )
     .setVersion('1.0')
     .addTag('Prosys')
     .build();
