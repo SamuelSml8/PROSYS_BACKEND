5bcd784281fa0806afd3de85f8f600363cd7ccbd
Samuel Sml <veramirandasamuel6@gmail.com>
2024-06-07 16:33:49 -0500

    docs: add swagger decorators to documentation

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -5,16 +5,20 @@ import { Tokens } from '../types/toke.type';
 import { LoginDto } from '../dtos/login-user.dto';
 import { RegisterDto } from '../dtos/register-user.dto';
 import { User } from 'src/modules/users/entities/user.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'User login' })
   @Post('login')
   async login(@Body() loginDto: LoginDto): Promise<ObjectResponse<Tokens>> {
     return this.authService.login(loginDto);
   }
 
+  @ApiOperation({ summary: 'User registration' })
   @Post('register')
   async register(
     @Body() registerDto: RegisterDto,
@@ -22,6 +26,7 @@ export class AuthController {
     return this.authService.register(registerDto);
   }
 
+  @ApiOperation({ summary: 'User logout' })
   @Post('logout')
   async logout(@Req() req): Promise<ObjectResponse<null>> {
     const token = req.headers.authorization.split(' ')[1];
