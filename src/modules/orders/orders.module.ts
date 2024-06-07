cc7f55124c02915ff77aad50226b817b7cb672b7
Samuel Sml <veramirandasamuel6@gmail.com>
2024-06-07 07:50:30 -0500

    build: order module configuration

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { OrderService } from './services/order.service';
+import { OrderController } from './controllers/order.controller';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Product } from '../products/entities/product.entity';
+import { OrderDetail } from './entities/order-detail.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Order, OrderDetail, Product])],
+  controllers: [OrderController],
+  providers: [OrderService],
+})
 export class OrdersModule {}
