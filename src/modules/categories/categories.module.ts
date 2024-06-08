9aeb9f287f841b76a1d4c5a67dc157ae2cd0712f
Samuel Sml <veramirandasamuel6@gmail.com>
2024-06-07 20:10:07 -0500

    fix: dependencies defined and auth module imported to use guard in categories.module

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -3,11 +3,13 @@ import { CategoryController } from './controllers/category.controller';
 import { CategoryService } from './services/category.service';
 import { Category } from './entities/category.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TokenService } from 'src/auth/services/token.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category]), AuthModule],
   controllers: [CategoryController],
-  providers: [CategoryService],
+  providers: [CategoryService, TokenService],
   exports: [],
 })
 export class CategoriesModule {}
